d723d32f17621085d9f01e7d9470ce58e86228d8
Gabbuz <120514689+GabKongroo@users.noreply.github.com>
2025-06-13 05:53:25 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,16 @@ from discord.ext import commands
 import asyncio
 from cogs.music import music_controller  
 from dotenv import load_dotenv
+import subprocess, logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s:%(message)s')
+
+try:
+    result = subprocess.run(["ffmpeg", "-version"], capture_output=True, text=True, check=True)
+    logging.info("FFmpeg trovato: " + result.stdout.splitlines()[0])
+except Exception as e:
+    logging.error("FFmpeg NON trovato o non funziona: " + str(e))
+
+
 
 # Carica variabili d'ambiente da .env (solo in locale)
 load_dotenv()
